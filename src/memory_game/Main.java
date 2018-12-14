827c5aaacb8c7a61f61a426d4a4ebbd0d68122cd
LucieWab <45763701+LucieWab@users.noreply.github.com>
2018-12-14 10:11:39 +0100

    main with error: right part of the circles are not clickable

--- src/memory_game/Main.java
+++ src/memory_game/Main.java
@@ -7,12 +7,18 @@ import java.util.List;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -21,16 +27,23 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
+
 public class Main extends Application{
 	private static final int NUM_OF_PAIRS = 8;
 	private static final int NUM_PER_ROW = 4;
 	private Tile selected = null;
 	private int clickCount = 2; //Number of mouse clicks allowed
+	
 
 	private Parent createContent() {
-		Pane root = new Pane();
-		root.setPrefSize(300, 300);
 		
+		final GridPane gridPane = new GridPane();
+		gridPane.setPadding(new Insets (7,7,7,7));
+		
+		gridPane.setHgap(25);
+		gridPane.setVgap(25);		
+		
+
 		//Create ArrayList of Tiles
 		int c = 0;	
 		List<Tile> tiles = new ArrayList<>();
@@ -39,34 +52,42 @@ public class Main extends Application{
 			tiles.add(new Tile(c));
 			c++;
 		}
-		
+
 
 		//Shuffle the list and place the tiles in X and Y directions
 		Collections.shuffle(tiles);
 		for (int i = 0; i < tiles.size(); i++) {		
 			Tile tile = tiles.get(i);
-			tile.setTranslateX(50 * (i % NUM_PER_ROW));
-			tile.setTranslateY(50 * (i / NUM_PER_ROW));							
-			root.getChildren().add(tile);			
+			tile.setTranslateX(60 * (i % NUM_PER_ROW));
+			tile.setTranslateY(60 * (i / NUM_PER_ROW));							
+			gridPane.getChildren().add(tile);			
 		}
-
-		return root;
+		gridPane.setStyle("    -fx-background-color: blue");
+		gridPane.getChildren().addAll(createResumeButton(),createExitButton());
+		return gridPane;
 
 	}
 
-	public void start(Stage primaryStage) {
-		primaryStage.setScene(new Scene(createContent()));
+	public void start(final Stage primaryStage) {
+		final BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(7, 7, 7, 7));
+		borderPane.setTop(createTopPane());
+		borderPane.setCenter(createContent());
+		borderPane.setLeft(createLeftPane());
+		primaryStage.setScene(new Scene(borderPane,800,500));	
 		primaryStage.show();
 	}	
-	
-private class Tile extends StackPane {
+
+	private class Tile extends StackPane {
 		
 		private Text text = new Text();
-		private Circle circle = new Circle(50, 50, 20);
+		
+		private Circle circle = new Circle(0, 0, 20);
 
 		// Method to create a tile
 		public Tile(int value) {
 			
+
 			/* HELP! (show the pairs with colors)
 			if (value == 0) {circle.setFill(Color.BLUE);}
 			if (value == 1) {circle.setFill(Color.AQUA);}
@@ -76,7 +97,7 @@ private class Tile extends StackPane {
 			if (value == 5) {circle.setFill(Color.GREEN);}
 			if (value == 6) {circle.setFill(Color.ORCHID);}
 			if (value == 7) {circle.setFill(Color.PERU);}*/
-			
+
 
 			circle.setStroke(Color.BLACK);
 			circle.setFill(Color.BISQUE);
@@ -86,13 +107,16 @@ private class Tile extends StackPane {
 
 			setAlignment(Pos.CENTER);
 			getChildren().addAll(circle, text);
-
-			setOnMouseClicked(this::handleMouseClick);
+			
+			text.setOnMouseClicked(this::handleMouseClick);
+			circle.setOnMouseClicked(this::handleMouseClick);
+		
 			close();
 		}
 
 		//Create MouseClick eventHandler
 		public void handleMouseClick(MouseEvent event) {
+			System.out.println("MouseClick");
 			if (isOpen() || clickCount == 0)
 				return;
 
@@ -119,7 +143,7 @@ private class Tile extends StackPane {
 		public boolean isOpen() {
 			return text.getOpacity() == 1;
 		}
-		
+
 		//Create fading transition from invisible to visible
 		public void open(Runnable action) {
 			FadeTransition ft = new FadeTransition(Duration.seconds(0.5), text);			
@@ -127,7 +151,7 @@ private class Tile extends StackPane {
 			ft.setOnFinished(e -> action.run());			
 			ft.play();
 		}
-		
+
 		//Create fading transition from visible to invisible
 		public void close() {
 			FadeTransition ft = new FadeTransition(Duration.seconds(0.5), text);
@@ -139,10 +163,79 @@ private class Tile extends StackPane {
 		public boolean hasSameValue(Tile other) {
 			return text.getText().equals(other.text.getText());
 		}
-		
+
 		//Check if the game is ended
 	}
 
+	private Button createResumeButton() {
+		
+		Button resume = new Button("RESUME");
+		resume.setTranslateX(15);
+		resume.setTranslateY(225);
+		resume.setStyle("    -fx-background-color: \r\n" + 
+				"        #090a0c,\r\n" + 
+				"        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\r\n" + 
+				"        linear-gradient(#20262b, #191d22),\r\n" + 
+				"        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\r\n" + 
+				"    -fx-background-radius: 5,4,3,5;\r\n" + 
+				"    -fx-background-insets: 0,1,2,0;\r\n" + 
+				"    -fx-text-fill: white;\r\n" + 
+				"    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\r\n" + 
+				"    -fx-font-family: \"Arial\";\r\n" + 
+				"    -fx-text-fill: linear-gradient(white, #d0d0d0);\r\n" + 
+				"    -fx-font-size: 12px;\r\n" + 
+				"    -fx-padding: 10 20 10 20;");
+
+		resume.setTextFill(Color.AQUA);
+		resume.setOnAction(event -> System.exit(0));
+		return resume;
+
+	}
+
+	private Button createExitButton() {
+		
+		Button exit = new Button("   EXIT   ");
+		
+		exit.setTranslateX(150);
+		exit.setTranslateY(225);
+		exit.setStyle("    -fx-background-color: \r\n" + 
+				"        #090a0c,\r\n" + 
+				"        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\r\n" + 
+				"        linear-gradient(#20262b, #191d22),\r\n" + 
+				"        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\r\n" + 
+				"    -fx-background-radius: 5,4,3,5;\r\n" + 
+				"    -fx-background-insets: 0,1,2,0;\r\n" + 
+				"    -fx-text-fill: white;\r\n" + 
+				"    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\r\n" + 
+				"    -fx-font-family: \"Arial\";\r\n" + 
+				"    -fx-text-fill: linear-gradient(white, #d0d0d0);\r\n" + 
+				"    -fx-font-size: 12px;\r\n" + 
+				"    -fx-padding: 10 20 10 20;");
+		exit.setTextFill(Color.AQUA);
+		exit.setOnAction(event -> System.exit(0));
+		return exit;
+
+	}
+
+	private VBox createLeftPane() {
+		VBox leftBox = new VBox(10);
+		Label l1 = new Label("										");
+		leftBox.setStyle("-fx-border-width: 2;-fx-border-color: green; -fx-background-color: green");	
+		leftBox.getChildren().add(l1);
+		return leftBox;
+		
+	}
+		
+	private Pane createTopPane() {
+		final VBox topBox = new VBox(25);
+		for (int i =0; i<3; i++) {
+			Label l1 = new Label("");
+			topBox.getChildren().add(l1);
+		}
+		topBox.setStyle("-fx-border-width: 2;-fx-border-color: red; -fx-background-color: red");			
+		return topBox;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
