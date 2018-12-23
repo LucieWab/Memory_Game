e52144ca5abe4125b9aea7353ddc87e89cd8c99c
LucieWab <45763701+LucieWab@users.noreply.github.com>
2018-12-23 11:17:13 +0100

    the final programm is here !

--- src/memory_game/Game.java
+++ src/memory_game/Game.java
@@ -1,6 +1,7 @@
 package memory_game;
 
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -21,6 +23,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -30,23 +34,112 @@ import javafx.util.Duration;
 
 
 
-public class Main extends Application{
+public class Game extends Application{
+	//Create the variables that we need for the programm
 	private static final int NUM_OF_PAIRS = 8;
 	private static final int NUM_PER_ROW = 4;
 	private Tile selected = null;
 	private int clickCount = 2; //Number of mouse clicks allowed
-	private int counter = 0;
+	private int counter = 0; //increase when a pair is founded
+	private Stage stage;
 
-	
+	public Scene startScene()
+	{
+		//Set the music from a .mp3 file
+		String uriString = new File("src/memory_game/theme.mp3").toURI().toString();
+		    MediaPlayer player = new MediaPlayer( new Media(uriString));
+		    player.play();
+		    player.setAutoPlay(true);
+		    
+		//Create the Pane withe the Labels and the Buttons    
+		Pane root = new Pane();
+		Label nameGame = new Label("****THE MEMORY'S GAME****");
+
+		Label instruction = new Label("\r\n" + 
+				"    The objective is to collect every pairs of numbers.\r\n" + "\r\n" +
+				"    Click on two circles to see if the numbers are the same. \r\n" + "\r\n" +
+				"    If you successfully match a pair the numbers will remain visible\r\n" + "\r\n" +
+				"    When you click on two circles that do not match, the numbers will disappear.\r\n" + "\r\n" +
+				"    The trick is to remember which numbers are where.\r\n" + "\r\n" +
+				"    You win when you find every pairs of numbers.\r\n" + "\r\n" +
+				"    GOOD LUCK");
+
+
+		Button button1 = new Button("START THE GAME");
+		button1.setOnAction( e-> stage.setScene(gameScene()));
+
+		nameGame.setLayoutX(150);
+
+		instruction.setLayoutX(200);
+		instruction.setLayoutY(75);
+
+		button1.setLayoutX(300);
+		button1.setLayoutY(450);
+
+		nameGame.setFont(Font.font ("Century Schoolbook", 30));
+		instruction.setFont(Font.font ("Century Schoolbook", 15));
+		button1.setStyle("    -fx-background-color: \r\n" + 
+				"        #090a0c,\r\n" + 
+				"        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\r\n" + 
+				"        linear-gradient(#20262b, #191d22),\r\n" + 
+				"        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\r\n" + 
+				"    -fx-background-radius: 5,4,3,5;\r\n" + 
+				"    -fx-background-insets: 0,1,2,0;\r\n" + 
+				"    -fx-text-fill: white;\r\n" + 
+				"    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\r\n" + 
+				"    -fx-font-family: \"Arial\";\r\n" + 
+				"    -fx-text-fill: linear-gradient(white, #d0d0d0);\r\n" + 
+				"    -fx-font-size: 12px;\r\n" + 
+				"    -fx-padding: 10 20 10 20;");
+
+		nameGame.setTextFill(Color.DARKSLATEBLUE);
+		instruction.setTextFill(Color.CADETBLUE);
+		button1.setTextFill(Color.CHOCOLATE);
+
+		root.getChildren().addAll(button1, nameGame, instruction);
+		button1.setPadding(new Insets(7, 7, 7, 7));
+
+		return new Scene(root,850,600);
+	}
+
+	public Scene endScene()
+	{
+		//Set the end music		
+		 String uriString = new File("src/memory_game/tortuga.mp3").toURI().toString();
+				    MediaPlayer player = new MediaPlayer( new Media(uriString));
+				    player.setAutoPlay(true);
+
+		//Create the pane with the Labels and add the Buttons
+		Pane root = new Pane();
+		Label message = new Label("CONGRATULATION !");
+		message.setLayoutY(150);
+		message.setFont(Font.font ("Century Schoolbook", 30));
+		message.setTextFill(Color.DARKSLATEBLUE);
+		root.getChildren().addAll(createResumeButton(), createExitButton(), message);
+		
+		return new Scene(root,325,400);
+	}
+
+	public Scene gameScene() {
+
+		//Create the BorderPane and return the Scene
+		final BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(7, 7, 7, 7));
+		borderPane.setTop(createTopPane());
+		borderPane.setCenter(createContent());
+		borderPane.setLeft(createLeftPane());	
+		return new Scene(borderPane,850,600);
+	}
 
 	public Parent createContent() {
-		
+
+		//Create the GridPane with the Tiles
 		final GridPane gridPane = new GridPane();
 		gridPane.setPadding(new Insets (7,7,7,7));
-		
+
 		gridPane.setHgap(25);
 		gridPane.setVgap(25);		
-		
+
 
 		//Create ArrayList of Tiles
 		int c = 0;	
@@ -67,41 +160,19 @@ public class Main extends Application{
 			gridPane.getChildren().add(tile);			
 		}
 		gridPane.getChildren().addAll(createResumeButton(),createExitButton());
+		gridPane.setCursor(Cursor.OPEN_HAND);
 		return gridPane;
 
 	}
 
-	public void start(final Stage primaryStage) {
-		final BorderPane borderPane = new BorderPane();
-		borderPane.setPadding(new Insets(7, 7, 7, 7));
-		borderPane.setTop(createTopPane());
-		borderPane.setCenter(createContent());
-		borderPane.setLeft(createLeftPane());
-		primaryStage.setScene(new Scene(borderPane,800,500));	
-		primaryStage.show();
-			
-	}	
-
 	private class Tile extends StackPane {
-		
+
 		private Text text = new Text();
-		
+
 		private Circle circle = new Circle(0, 0, 20);
 
 		// Method to create a tile
 		public Tile(int value) {
-			
-
-			// HELP! (show the pairs with colors)
-			/*if (value == 0) {circle.setFill(Color.BLUE);}
-			if (value == 1) {circle.setFill(Color.AQUA);}
-			if (value == 2) {circle.setFill(Color.RED);}
-			if (value == 3) {circle.setFill(Color.GOLD);}
-			if (value == 4) {circle.setFill(Color.GRAY);}
-			if (value == 5) {circle.setFill(Color.GREEN);}
-			if (value == 6) {circle.setFill(Color.ORCHID);}
-			if (value == 7) {circle.setFill(Color.PERU);}*/
-
 
 			circle.setStroke(Color.BLACK);
 			circle.setFill(Color.DARKSLATEBLUE);
@@ -112,16 +183,16 @@ public class Main extends Application{
 
 			setAlignment(Pos.CENTER);
 			getChildren().addAll(circle, text);
-			
+
 			text.setOnMouseClicked(this::handleMouseClick);
 			circle.setOnMouseClicked(this::handleMouseClick);
-		
+
 			close();
 		}
 
 		//Create MouseClick eventHandler
 		public void handleMouseClick(MouseEvent event) {
-			System.out.println("MouseClick - " + counter);
+
 			if (isOpen() || clickCount == 0)
 				return;
 
@@ -136,13 +207,13 @@ public class Main extends Application{
 					if (!hasSameValue(selected)) {
 						selected.close();
 						this.close();
-						
+
 					}
 					else {counter++;}
 					selected = null;
 					clickCount = 2;
 					endGame();
-					
+
 				});
 			}
 		}
@@ -173,19 +244,15 @@ public class Main extends Application{
 		}
 
 		//Check if the game is ended
-		public Stage endGame() {
-			if (counter == 8) {
-				System.out.println("End Game");
-				return new Stage();}
-			else {return null;}
-			
+		public void endGame() {
+			if (counter == NUM_OF_PAIRS) {
+				stage.setScene(endScene());
+			}
 		}
-			
-		
 	}
 
 	private Button createResumeButton() {
-		
+
 		Button resume = new Button("RESUME");
 		resume.setTranslateX(15);
 		resume.setTranslateY(325);
@@ -204,22 +271,20 @@ public class Main extends Application{
 				"    -fx-padding: 10 20 10 20;");
 
 		resume.setTextFill(Color.AQUA);
-		
-		resume.setOnAction(new EventHandler<ActionEvent>(){
-			public void handle(ActionEvent event) {
-				counter = 0;
-				// Go back to the start scene
-				}
-		}
-				);
+
+		resume.setOnAction(e -> {
+			counter =0;
+			stage.setScene(gameScene());
+			});
+					
 		return resume;
 
 	}
 
 	private Button createExitButton() {
-		
+
 		Button exit = new Button("   EXIT   ");
-		
+
 		exit.setTranslateX(230);
 		exit.setTranslateY(325);
 		exit.setStyle("    -fx-background-color: \r\n" + 
@@ -244,24 +309,33 @@ public class Main extends Application{
 	private VBox createLeftPane() {
 		VBox leftBox = new VBox(10);
 		Label l1 = new Label("								");
-		//leftBox.setStyle("-fx-border-width: 2;-fx-border-color: green; -fx-background-color: green");	
 		leftBox.getChildren().add(l1);
 		return leftBox;
-		
+
 	}
-		
+
 	private Pane createTopPane() {
 		final VBox topBox = new VBox(25);
 		for (int i =0; i<2; i++) {
 			Label l1 = new Label("");
 			topBox.getChildren().add(l1);
 		}
-		//topBox.setStyle("-fx-border-width: 2;-fx-border-color: red; -fx-background-color: red");			
 		return topBox;
 	}
+
+	public void start(final Stage primaryStage) throws Exception {
+		stage = primaryStage;
+		primaryStage.setScene(startScene());	
+		primaryStage.setTitle("Memory Game");
+		primaryStage.show();
+
+	}	
+
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+
 
 }
---
+++ src/memory_game/MainTest.java
@@ -0,0 +1,15 @@
+package memory_game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public final void testMain() {
+		fail("Not yet implemented");
+	}
+	 
+	
+}
